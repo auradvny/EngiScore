f5b51b4068f5a853a2512813eb1ac73102b24cf9
calistaandt <calistaandta@gmail.com>
2024-05-23 20:56:53 +0700

    edit sql

--- sistempoint.sql
+++ sistempoint.sql
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel tb_mhs
+-- Struktur dari tabel tb_mahasiswa
 --
 
 CREATE TABLE tb_mhs (
