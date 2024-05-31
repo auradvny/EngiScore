cc5a0906b6dfbed15fa27b6734dc3114f65aeaf2
calistaandt <calistaandta@gmail.com>
2024-05-31 21:04:48 +0700

    baru

--- application/views/bapendik/index.php
+++ application/views/bapendik/index.php
@@ -29,7 +29,7 @@
 
                         <div class="info-box-content">
                             <span class="info-box-text">Jumlah Permohonan</span>
-                            <span class="info-box-number">41,410</span>
+                            <span class="info-box-number">41</span>
                         </div>
                         <!-- /.info-box-content -->
                     </div>
