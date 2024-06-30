2482312e03a798da8db81b3d8a4577d769e49157
auradvny <auradvny14@gmail.com>
2024-06-30 15:06:58 +0700

    revisi

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -113,41 +113,185 @@ class Auth extends CI_Controller
 				'tgl_dibuat' => time(),
 			];
 
-			//$this->db->insert('tb_user', $data);
+			$this->db->insert('tb_user', $data);
 
-			$this->_sendEmail();
+			// $email = $this->input->post('email');
+			// $token = base64_encode(random_bytes(32));
+			// $user_token = [
+			// 	'email' => $email,
+			// 	'token' => $token,
+			// 	'date_created' => time()
+			// ];
+
+			// $this->db->insert('tb_user_token', $user_token);
+
+			// $this->_sendEmail($token, 'verify');
 
 			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Akun berhasil dibuat!</div>');
 			redirect('auth');
 		}
 	}
 
-	private function _sendEmail()
-	{
-		$config = [
-			'protocol' => 'smtp',
-			'smtp_host' => 'ssl://smtp.googlemail.com',
-			'smtp_user' => 'radevans14@gmail.com',
-			'smtp_pass' => 'Coba_12345',
-			'smtp_port' => 456,
-			'mailtype' => 'html',
-			'charset' => 'utf-8',
-			'newline' => "\r\n"
+	// private function _sendEmail($token, $type)
+	// {
+	// 	$config = [
+	// 		'protocol' => 'smtp',
+	// 		'smtp_host' => 'ssl://smtp.googlemail.com',
+	// 		'smtp_user' => 'engiscore@gmail.com',
+	// 		'smtp_pass' => 'Coba_12345',
+	// 		'smtp_port' => 456,
+	// 		'mailtype' => 'html',
+	// 		'charset' => 'utf-8',
+	// 		'newline' => "\r\n"
 
-		];
+	// 	];
 
-		$this->load->library('email', $config);
-		$this->email->from('radevans14@gmail.com', 'Aura Devany');
-		$this->email->to('aura.bachtiar@mhs.unsoed.ac.id');
-		$this->email->subject('Tes');
-		$this->email->message('Hello');
-		if ($this->email->send()) {
-			return true;
-		} else {
-			echo $this->email->print_debugger();
-			die;
-		}
-	}
+	// 	$this->load->library('email', $config);
+	// 	$this->email->from('engiscore@gmail.com', 'Engine Score');
+	// 	$this->email->to($this->input->post('email'));
+
+	// 	if ($type == 'verify') {
+	// 		$this->email->subject('Account Verification');
+	// 		$this->email->message('Click this link to verify your account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Activate</a>');
+	// 	} else if ($type == 'forgot') {
+	// 		$this->email->subject('Reset Password');
+	// 		$this->email->message('Click this link to reset your password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+	// 	}
+
+	// 	if ($this->email->send()) {
+	// 		return true;
+	// 	} else {
+	// 		echo $this->email->print_debugger();
+	// 		die;
+	// 	}
+	// }
+
+	// public function verify()
+	// {
+	// 	$email = $this->input->get('email');
+	// 	$token = $this->input->get('token');
+
+	// 	$user = $this->db->get_where('tb_user', ['email' => $email, 'is_active' => 1])->row_array();
+	// 	if ($user) {
+	// 		$user_token = $this->db->get_where('tb_user_token', ['token' => $token])->row_array();
+
+	// 		if ($user_token) {
+	// 			if (time() - $user_token['date_created'] < (60 * 60 * 24)) {
+	// 				$this->db->set('is_active', 1);
+	// 				$this->db->where('email', $email);
+	// 				$this->db->update('tb_user');
+
+	// 				$this->db->delete('tb_user_token', ['email' => $email]);
+
+	// 				$this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">' . $email . ' has been activated! Please login.</div>');
+	// 				redirect('auth');
+	// 			} else {
+	// 				// $this->db->delete('tb_user', ['email' => $email]);
+	// 				$this->db->delete('tb_user_token', ['email' => $email]);
+
+	// 				$this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">
+	// 			Account acctivation failed! Token expired.</div>');
+	// 				redirect('auth');
+	// 			}
+	// 		} else {
+	// 			$this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">
+	// 			Account acctivation failed! Wrong token.</div>');
+	// 			redirect('auth');
+	// 		}
+	// 	} else {
+	// 		$this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">
+	//         Account acctivation failed! Wrong email.</div>');
+	// 		redirect('auth');
+	// 	}
+	// }
+
+	// public function forgotpassword()
+	// {
+	// 	$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+	// 	if ($this->form_validation->run() == false) {
+	// 		$data['title'] = 'Forgot Password';
+	// 		$this->load->view('templates/auth_header', $data);
+	// 		$this->load->view('auth/forgot-password');
+	// 		$this->load->view('templates/auth_footer');
+	// 	} else {
+	// 		$email = $this->input->post('email');
+	// 		$user = $this->db->get_where('tb_user', ['email' => $email, 'is_active' => 1])->row_array();
+	// 		if ($user) {
+	// 			$token = base64_encode(random_bytes(32));
+	// 			$user_token = [
+	// 				'email' => $email,
+	// 				'token' => $token,
+	// 				'date_created' => time()
+	// 			];
+
+	// 			$this->db->indert('user_token', $user_token);
+	// 			$this->_sendEmail($token, 'forgot');
+	// 			$this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">
+	// 			Please check your email to reset your password!</div>');
+	// 			redirect('auth/forgotpassword');
+	// 		} else {
+	// 			$this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">
+	//         Email is not registered or activated!</div>');
+	// 			redirect('auth/forgotpassword');
+	// 		}
+	// 	}
+	// }
+
+	// public function resetPassword()
+	// {
+	// 	$email = $this->input->get('email');
+	// 	$token = $this->input->get('token');
+
+	// 	$user = $this->db->get_where('tb_user', ['email' => $email])->row_array();
+
+	// 	if ($user) {
+	// 		$user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+	// 		if ($user_token) {
+	// 			$this->session->set_userdata('reset_email', $email);
+	// 			$this->changePassword();
+	// 		} else {
+	// 			$this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">
+	//         Reset password failed! Wrong email.</div>');
+	// 			redirect('auth');
+	// 		}
+	// 	} else {
+	// 		$this->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">
+	//         Reset password failed! Wrong email.</div>');
+	// 		redirect('auth');
+	// 	}
+	// }
+
+	// public function changePassword()
+	// {
+	// 	if (!$this->session->userdata('reser_email')) {
+	// 		redirect('auth');
+	// 	}
+
+	// 	$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[8]|callback_valid_password|matches[password2]', [
+	// 		'matches' => 'Password is not the same!',
+	// 		'min_length' => 'The password is too short!'
+	// 	]);
+	// 	$this->form_validation->set_rules('password2', 'Repeat Password', 'required|trim|matches[password1]');
+	// 	if ($this->form_validation->run() == false) {
+	// 		$data['title'] = 'Change Password';
+	// 		$this->load->view('templates/auth_header', $data);
+	// 		$this->load->view('auth/change-password');
+	// 		$this->load->view('templates/auth_footer');
+	// 	} else {
+	// 		$password = password_hash($this->input->post('password1'), PASSWORD_DEFAULT);
+	// 		$email = $this->session->userdata('reset_email');
+
+	// 		$this->db->set('pass', $password);
+	// 		$this->db->where('email', $email);
+	// 		$this->db->update('tb_user');
+
+	// 		$this->session->unset_userdata('reset_email');
+	// 		$this->session->set_flashdata('pesan', '<div class="alert alert-success" role="alert">
+	//        Password has been changed! Please login.</div>');
+	// 		redirect('auth');
+	// 	}
+	// }
 
 	public function logout()
 	{
